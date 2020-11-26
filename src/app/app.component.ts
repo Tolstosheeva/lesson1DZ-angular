8f92e75afb99a2a453f934ffad79a8e5706a1efd
Tolstosheeva <tolstosheeva.m.v@gmail.com>
2020-11-26 02:00:15 +0200

    lesson2dz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   list = [{
     name:'Уборка',
-    
+    isDone: true,
   },{name:'Покупка продуктов',
- 
+  isDone: false,
   },{
     name:'Поездка к врачу',
-    
+    isDone: true,
   },{name:'Приготовление ужина',
-  
+  isDone: true,
   },{
     name:'Организовать вечеринку',
-    
+    isDone: false,
   }]
   tasks = [{
    isDone: true,
   }]
   addTask() {
     this.list.push({
-      name:'организовать вечеринку'
-      
+      name:'организовать вечеринку',
+      isDone: true,
       
     })
   }
